247d1d8ee98d328a32e57f8cc6bab4d942686411
Angel <morante413@gmail.com>
2019-07-04 15:12:12 -0400

    new changues

--- src/hooks/useGetData.js
+++ src/hooks/useGetData.js
@@ -1,7 +1,20 @@
 import { useState, useEffect } from 'react';
-const api = 'https://us-central1-gndx-cv.cloudfunctions.net/me';
+import firebase from 'firebase';
 
+const api = 'https://us-central1-gndx-cv.cloudfunctions.net/me';
+const firebaseConfig = {
+  apiKey: "AIzaSyB7JR0y3RYI--acp9JtIV6TjSG4ET2RIIA",
+  authDomain: "angel-api-3bfff.firebaseapp.com",
+  databaseURL: "https://angel-api-3bfff.firebaseio.com",
+  projectId: "angel-api-3bfff",
+  storageBucket: "angel-api-3bfff.appspot.com",
+  messagingSenderId: "558216148272",
+  appId: "1:558216148272:web:330fe49c12d79b93"
+};
 const useGetData = () => {
+
+  firebase.initializeApp(firebaseConfig);
+  console.log(firebase)
   const [myData, setData] = useState([]);
 
   useEffect( () => {
